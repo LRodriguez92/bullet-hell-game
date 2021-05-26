70bd33f9a29db94e296b1e25dbd41e4b93ddcbe2
Leo <Leonardo.Rodriguez92@gmail.com>
2021-05-26 19:18:08 -0400

    can restart game

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
@@ -25,6 +26,11 @@ public class GameController : MonoBehaviour
     {
         loseScreen.SetActive(true);
         Time.timeScale = 0;
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(0);
     }
     
 }
